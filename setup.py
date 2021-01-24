d66ec4a89a6d2ebc7016633edf2a593ed6453d59
gnk-sato-mnx <g.sato@isdajp.com>
2021-01-24 13:43:18 +0900

    lambda@edge 対応

--- setup.py
+++ setup.py
@@ -4,27 +4,13 @@ from setuptools import setup
 from io import open
 from zappa import __version__
 
-# Set external files
-try:
-    from pypandoc import convert
-    README = convert('README.md', 'rst')
-except ImportError:
-    README = open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding="utf-8").read()
+with open('README.md') as readme_file:
+    long_description = readme_file.read()
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
-    if sys.version_info[0] == 2:
-        required = f.read().splitlines()
-    else:
-        # This logic is intended to prevent the futures package from being installed in python 3 environments
-        # as it can cause unexpected syntax errors in other packages. Futures is in the standard library in python 3
-        # and is should never be installed in these environments.
-        # Related: https://github.com/Miserlou/Zappa/issues/1179
-        required = []
-        for package in f.read().splitlines():
-            if 'futures' not in package:
-                required.append(package)
+with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as f:
+    required = f.read().splitlines()
 
-with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f:
+with open(os.path.join(os.path.dirname(__file__), 'test_requirements.in')) as f:
     test_required = f.read().splitlines()
 
 setup(
@@ -37,7 +23,8 @@ setup(
     include_package_data=True,
     license='MIT License',
     description='Server-less Python Web Services for AWS Lambda and API Gateway',
-    long_description=README,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Miserlou/Zappa',
     author='Rich Jones',
     author_email='rich@openwatch.net',
@@ -52,8 +39,13 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
